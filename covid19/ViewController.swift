f534e2353f7a4e77d4608eb7cc02de4b7e1f45ad
zulkufseckin <zulkuf.seckin@mobven.com>
2021-11-02 22:08:04 +0300

    Initial Commit

---
+++ covid19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covid19
+//
+//  Created by Z.Seckin on 2.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
